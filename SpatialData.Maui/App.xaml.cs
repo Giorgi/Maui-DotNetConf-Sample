902a18e06050a8ced5c15a04e4e87766c4a68713
Giorgi <giorgi@giorgi.dev>
2023-11-13 15:13:55 +0400

    Initial Commit

---
+++ SpatialData.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SpatialData.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
