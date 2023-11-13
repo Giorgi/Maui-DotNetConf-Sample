e6edb6be568661341ca7cb96df0dd7dd997401a4
Giorgi <giorgi@giorgi.dev>
2023-11-13 15:54:16 +0400

    Add and configure Maps package

--- SpatialData.Maui/MauiProgram.cs
+++ SpatialData.Maui/MauiProgram.cs
@@ -13,7 +13,7 @@ namespace SpatialData.Maui
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                }).UseMauiMaps();
 
 #if DEBUG
 		builder.Logging.AddDebug();
